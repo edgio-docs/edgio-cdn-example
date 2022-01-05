7a4690427f8a2fcf3156a2e533756e6dfe73a63e
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-01-05 19:54:21 +0530

    Update browser.ts

--- src/browser.ts
+++ src/browser.ts
@@ -2,7 +2,6 @@ import install from '@layer0/prefetch/window/install'
 import installDevtools from '@layer0/devtools/install'
 
 document.addEventListener('DOMContentLoaded', function () {
-  
   installDevtools()
   install()
 })
