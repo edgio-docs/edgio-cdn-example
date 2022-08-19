f07ec06cdea8eff995853f41d91358228e4a778d
Tristan Lee <tristanlee85@gmail.com>
2022-08-19 14:40:31 -0400

    Update location header domain to edg.io

--- src/route-handler.ts
+++ src/route-handler.ts
@@ -9,7 +9,7 @@ const handler: RouteHandler = async ({ cache, removeUpstreamResponseHeader, prox
 
   // convert absolute redirects to origin to relative
   // so that the user isn't transferred to the origin.
-  updateResponseHeader('location', /https:\/\/www\.layer0\.co\//gi, '/')
+  updateResponseHeader('location', /https:\/\/www\.edg\.io\//gi, '/')
 }
 
 export default handler
