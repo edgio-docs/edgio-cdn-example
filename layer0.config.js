b9ff138346e037e2163165ec994ec6c289e98d2c
Tristan Lee <tristanlee85@gmail.com>
2022-08-19 14:33:37 -0400

    Update backend to edgio home

--- layer0.config.js
+++ layer0.config.js
@@ -3,8 +3,8 @@ module.exports = {
   connector: "@layer0/starter",
   backends: {
     origin: {
-      domainOrIp: "www.layer0.co",
-      hostHeader: "www.layer0.co",
+      domainOrIp: "www.edg.io",
+      hostHeader: "www.edg.io",
     },
   },
 };
